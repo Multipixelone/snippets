af6da64a457e934641d0bb5b2fe6658e9565fc93
multipixelone <finn@cnwr.net>
2017-09-09 21:38:16 -0700

    Working

--- InstallRetroarch.sh
+++ InstallRetroarch.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # Made by Multipixelone
 echo "Retroarch Installer by Multipixelone. Also edits config appropriatly"
+if [ "$EUID" -ne 0 ]
+    then echo "Sorry, I need root access. Please run as root or with sudo ;)"
+    exit
+fi
 
+# Install Retroarch
+sudo add-apt-repository ppa:libretro/testing
+sudo apt-get update
+sudo apt-get install retroarch libretro-*
+
+# Install my config
+cp retroarch.cfg ~/.config/retroarch/
+
+# All done
+echo "All done, thanks~"
