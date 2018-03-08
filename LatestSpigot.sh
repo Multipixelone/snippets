8dc88217c84c84b54089c0dfdbd4d2acfb2e1475
Finn <finn@cnwr.net>
2018-03-07 23:07:34 -0800

    Update LatestSpigot.sh

--- LatestSpigot.sh
+++ LatestSpigot.sh
@@ -1,3 +1,3 @@
-sudo apt -y install openjdk-9-jre
+sudo apt -y install openjdk-9-jre git
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
 java -jar BuildTools.jar --rev 1.12.1
